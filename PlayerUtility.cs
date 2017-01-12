2da61af4b73704c92f7e95b36b2921cc257084f9
Tyler Lindberg <tlindberg@ucla.edu>
2017-01-12 15:37:17 -0800

    Changed getters/setters to properties. Also adjusted function names to be capitalized to conform with C# naming conventions.

--- PlayerUtility.cs
+++ PlayerUtility.cs
@@ -3,68 +3,56 @@ using System.Collections;
 
 public class PlayerUtility : MonoBehaviour {
 
+	////////////////////////////////////////////
+	// Player Properties
+	////////////////////////////////////////////
+
 	private Vector3 spawnPoint;
+	public Vector3 SpawnPoint {
+		get { return spawnPoint; }
+		set { spawnPoint = value; }
+	}
 
 	private bool isDead;
+	public bool IsDead {
+		get { return isDead; }
+		set { isDead = true; }
+	}
 
 	private float points;
+	public float Points {
+		get { return points; }
+		set { points = value; }
+	}
 
 	private int team;
+	public int Team {
+		get { return team; }
+		set { team = value; }
+	}
+
+	public Vector3 Location {
+		get { return transform.position; }
+	}
+
+	public Vector3 Velocity {
+		get { return rb.velocity; }
+	}
+
+	////////////////////////////////////////////
+	// Player Components
+	////////////////////////////////////////////
 
 	private Rigidbody rb;
+
+
 	void Start()
 	{
 		rb = gameObject.GetComponentInChildren<Rigidbody>();
 	}
 
-	void setSpawn(Vector3 spawn)
-	{
-		spawnPoint = spawn;
-	}
-
-	void respawn()
+	public void Respawn()
 	{
 		gameObject.transform.position = spawnPoint;
 	}
-
-	float getPoints()
-	{
-		return points;
-	}
-
-	void setPoints(float p)
-	{
-		points = p;
-	}
-
-	void addPoints(float p)
-	{
-		points += p;
-	}
-
-	Vector3 getLocation()
-	{
-		return transform.position;
-	}
-
-	Vector3 getSpeed()
-	{
-		return rb.position;
-	}
-
-	int getTeam()
-	{
-		return team;
-	}
-
-	void setTeam(int t)
-	{
-		team = t;
-	}
-
-	void setDead(bool state)
-	{
-		isDead = state;
-	}
-
 }
