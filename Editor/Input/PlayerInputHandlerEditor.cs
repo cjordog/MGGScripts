f45d5042305f51d361cdbfc8ec2250f121e5808c
Tyler Lindberg <tlindberg@ucla.edu>
2017-01-12 16:47:55 -0800

    Added player input scripts
    Added a script for player input that can be used to get controller axes
    and buttons as well as keyboard presses. Also added Editor script to
    hide editor controls for PlayerInputHandler during play mode.

---
+++ Editor/Input/PlayerInputHandlerEditor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PlayerInputHandler))]
+[CanEditMultipleObjects]
+public class PlayerInputHandlerEditor : Editor {
+
+	public override void OnInspectorGUI() {
+		if(!Application.isPlaying) {
+			DrawDefaultInspector();
+		}
+	}
+}
---
+++ Scripts/Player/PlayerInputHandler.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputHandler : MonoBehaviour {
+
+    struct MovementAxes
+    {
+        public string horizontal;
+        public string vertical;
+
+        public MovementAxes(string h, string v)
+        {
+            horizontal = h;
+            vertical = v;
+        }
+
+        public static void Swap(ref MovementAxes a, ref MovementAxes b)
+        {
+            MovementAxes temp = b;
+            b = a;
+            a = temp;
+        }
+    }
+
+    struct ActionButtons
+    {
+        public string action1;
+        public string action2;
+
+        public ActionButtons(string a1, string a2)
+        {
+            action1 = a1;
+            action2 = a2;
+        }
+
+        public static void Swap(ref ActionButtons a, ref ActionButtons b)
+        {
+            ActionButtons temp = b;
+            b = a;
+            a = temp;
+        }
+    }
+
+
+    [Tooltip("The \"number\" of the player. " +
+        "Represents which controller or keyboard controls to use. " +
+        "May not correspond to who is considered \"first player.\"")]
+    [Range(1, 4)]
+    public uint playerNumber = 1;
+
+    [Tooltip("Whether or not this player's input should be gotten " +
+        "from a controller")]
+    public bool useController = true;
+
+    private MovementAxes movement;
+    private ActionButtons actions;
+
+
+    void Awake()
+    {
+        Reset();
+    }
+
+    public float GetHorizontal()
+    {
+        return Input.GetAxis(movement.horizontal);
+    }
+
+    public float GetVertical()
+    {
+        return Input.GetAxis(movement.vertical);
+    }
+
+    public bool GetAction1()
+    {
+        return Input.GetButtonDown(actions.action1);
+    }
+
+    public bool GetAction2()
+    {
+        return Input.GetButtonDown(actions.action2);
+    }
+
+    public void Reset()
+    {
+        string suffix = useController ? "-GP" : "";
+
+        movement.horizontal = "LeftHorizontal-" + playerNumber +
+            suffix;
+        movement.vertical = "LeftVertical-" + playerNumber +
+            suffix;
+
+        actions.action1 = "Action1-" + playerNumber + suffix;
+        actions.action2 = "Action2-" + playerNumber + suffix;
+    }
+
+    private void SwapMovementAxes(ref MovementAxes other)
+    {
+        MovementAxes.Swap(ref movement, ref other);
+    }
+
+    private void SwapActionButtons(ref ActionButtons other)
+    {
+        ActionButtons.Swap(ref actions, ref other);
+    }
+}
