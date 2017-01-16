3add1659830dfcf0f81df4548caecd72abd045c5
Tyler Lindberg <tlindberg@ucla.edu>
2017-01-15 20:38:25 -0800

    Player controls can now be changed from editor at runtime

--- Editor/Input/PlayerInputHandlerEditor.cs
+++ Editor/Input/PlayerInputHandlerEditor.cs
@@ -7,17 +7,29 @@ using UnityEditor;
 [CanEditMultipleObjects]
 public class PlayerInputHandlerEditor : Editor {
 
+	private uint playerNum;
+	private bool state1;
+	private uint previousNum;
+	private bool useController;
+	private string controlString;
+
 	public override void OnInspectorGUI() {
-		if(!Application.isPlaying) {
-			DrawDefaultInspector();
+		PlayerInputHandler script = (PlayerInputHandler)target;
+		// controlString = useController ? "Controller" : "Keyboard";
+
+		DrawDefaultInspector();
+
+		if(Application.isPlaying) {
+			if(script.playerNumber != playerNum ||
+				script.useController != useController) {
+				script.Reset();
+				playerNum = script.playerNumber;
+				useController = script.useController;
+			}
 		}
 		else {
-			PlayerInputHandler script = (PlayerInputHandler)target;
-			GUILayout.Label("Player Number: " + script.playerNumber);
-			if(script.useController)
-				GUILayout.Label("Player is using controller");
-			else
-				GUILayout.Label("Player is not using controller");
+			playerNum = script.playerNumber;
+			useController = script.useController;
 		}
 	}
 }
