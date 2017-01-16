6f546ec0d76cf7b807f59838a94f2a6715e780b0
Tyler Lindberg <tlindberg@ucla.edu>
2017-01-15 17:33:07 -0800

    Now unable change actions during runtime
    Added Editor script to only draw labels showing the name of each action
    while the game is playing.

---
+++ Editor/Actions/PlayerActionHandlerEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PlayerActionHandler))]
+[CanEditMultipleObjects]
+public class PlayerActionHandlerEditor : Editor {
+
+	public override void OnInspectorGUI() {
+		if(Application.isPlaying) {
+			PlayerActionHandler script = (PlayerActionHandler)target;
+			GUILayout.Label("Action 1: " + script.action1);
+			GUILayout.Label("Action 2: " + script.action2);
+		}
+		else {
+			DrawDefaultInspector();
+		}
+	}
+}
