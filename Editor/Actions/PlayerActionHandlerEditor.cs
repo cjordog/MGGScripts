83fd41b68cd5500d8144bf76d8096b52a5e88a15
Tyler Lindberg <tlindberg@ucla.edu>
2017-01-15 20:01:56 -0800

    Player actions can now be changed from editor at runtime
    Editor script was edited to call SetAction[12s] from
    PlayerActionHandler when a change is detected in an ActionType variable.

--- Editor/Actions/PlayerActionHandlerEditor.cs
+++ Editor/Actions/PlayerActionHandlerEditor.cs
@@ -7,14 +7,26 @@ using UnityEditor;
 [CanEditMultipleObjects]
 public class PlayerActionHandlerEditor : Editor {
 
+	private Actions.ActionType a1;
+	private Actions.ActionType a2;
+
 	public override void OnInspectorGUI() {
+		DrawDefaultInspector();
+		PlayerActionHandler script = (PlayerActionHandler)target;
+
 		if(Application.isPlaying) {
-			PlayerActionHandler script = (PlayerActionHandler)target;
-			GUILayout.Label("Action 1: " + script.action1);
-			GUILayout.Label("Action 2: " + script.action2);
+			if(script.action1 != a1) {
+				script.SetAction1(script.action1);
+				a1 = script.action1;
+			}
+			if(script.action2 != a2) {
+				script.SetAction2(script.action2);
+				a2 = script.action2;
+			}
 		}
 		else {
-			DrawDefaultInspector();
+			a1 = script.action1;
+			a2 = script.action2;
 		}
 	}
 }
