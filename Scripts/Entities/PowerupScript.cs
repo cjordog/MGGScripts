7bc5377acfecbd60c4528937ab2a286cc227ee3a
bricksorcerer <corbintsmart@gmail.com>
2017-01-18 19:27:21 -0800

    Destroy powerups on collision

--- Scripts/Entities/PowerupScript.cs
+++ Scripts/Entities/PowerupScript.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class PowerupScript : MonoBehaviour {
 
+	private float powerupHealth = 1f;
+
 	void OnTriggerEnter(Collider other) {
-		Debug.Log ("collide");
-		if (other.tag == "Player")
+		if (other.tag == "Player") {
+			other.gameObject.GetComponentInParent <PlayerUtility> ().addHealth (powerupHealth);
 			Destroy (gameObject);
+		}
 	}
 }
