afa2de7bb7b6c25fb5e010c71e6208f2d3e6e1e3
Tyler Lindberg <tlindberg@ucla.edu>
2017-01-25 19:25:24 -0800

    Added mapping input to player number
    InputMapper will get all current pressed buttons and allocate
    add/remove players from GlobalData based on that.

---
+++ Scripts/Input/InputMapper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputMapper : MonoBehaviour {
+
+	private GlobalData data;
+	private List<PlayerInputHandler.ControlSettings> controlSettings;
+
+	void Start() {
+		data = GameObject.FindGameObjectWithTag("Data").GetComponent<GlobalData>();
+	}
+
+	void Update() {
+		controlSettings = PlayerInputHandler.GetAllAction1();
+		for(int i = 0; i < controlSettings.Count; i++) {
+			data.AddPlayerData(controlSettings[i]);
+		}
+
+		controlSettings = PlayerInputHandler.GetAllAction2();
+		for(int i = 0; i < controlSettings.Count; i++) {
+			data.RemovePlayerData(controlSettings[i]);
+		}
+	}
+}
--- Scripts/Miscellaneous/GlobalData.cs
+++ Scripts/Miscellaneous/GlobalData.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class GlobalData : MonoBehaviour {
 
-	//public for testing, should be private
 	private int numPlayers = 0;
 	public int NumPlayers {
 		get { return numPlayers; }
@@ -28,10 +27,23 @@ public class GlobalData : MonoBehaviour {
 			initialized = true;
 			DontDestroyOnLoad(gameObject);
 		}
-	}   
+	}
+
+	public int AddPlayerData(PlayerInputHandler.ControlSettings controlSettings) {
+		return AddPlayerData(controlSettings.num, controlSettings.useController);
+	}
 
 	public int AddPlayerData(int controlNum, bool useController = false) {
-		if(numPlayers < 3 && numPlayers >= 0) {
+		// Check if player is already active, and return if it is
+		for(int i = 0; i < numPlayers; i++) {
+			if(players[i].controlNum == controlNum &&
+				players[i].useController == useController) {
+				return -1;
+			}
+		}
+
+		if(numPlayers < 4 && numPlayers >= 0) {
+			// Set Player data and add player
 			players[numPlayers].points = 0;
 			players[numPlayers].hats = new string[5];
 			players[numPlayers].controlNum = controlNum;
@@ -43,6 +55,20 @@ public class GlobalData : MonoBehaviour {
 			Debug.LogError("Maximum Players Reached: 4");
 			return -1;
 		}
+	}
+
+	public bool RemovePlayerData(PlayerInputHandler.ControlSettings controlSettings) {
+		return RemovePlayerData(controlSettings.num, controlSettings.useController);
+	}
+
+	public bool RemovePlayerData(int controlNum, bool useController) {
+		for(int i = 0; i < players.Length; i++) {
+			if(controlNum == players[i].controlNum &&
+				useController == players[i].useController) {
+				return RemovePlayerData(i);
+			}
+		}
+		return false;
 	}
 
 	public bool RemovePlayerData(int playerNum)
